8a43215780c2572e52dca6110718e1b8bc498fbe
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-03-31 01:33:56 -0400

    comment code and add TO-DOs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
 function initMap(){
-		firebase.database().ref('playgrounds/').on("value", function(snapshot) {
+	firebase.database().ref('playgrounds/').on("value", function(snapshot) {
 
 		// Use equalTo() with lat/lng bounds to choose arbitrary starting, ending, and equivalence points for queries.
 		// This can be useful for paginating data or finding items with children that have a specific value.
+
 		var locations = snapshot.val();
-		// var location = locations['-alison-hardin-playground-eli-bradford-garden-of-angels-at-jo-kelly-school-fort-worth-tx'];
+		// collect Place for each location from createMarkers
 		var places = [];
 
 		var Place = function(data, map) {
@@ -15,51 +16,58 @@ function initMap(){
 				map: map,
 				position: data.location,
 				animation: google.maps.Animation.DROP,
-				name: data.name
+				name: data.name,
+				address: data.address,
+				city: data.city,
+				state: data.state,
+				zip: data.zip,
+				location: data.location,
+				url: data.utl,
+				features: data.features
 			});
 			self.marker.addListener('click', function(){
-				console.log(this);
+				console.log(this.name, this.city, this.location);
+
+				// TO-DO: figure out how to launch details screen ob click
 			});
 
-			//geocoding query... why did I put this here? IDK I'm tired.
+			//geocoding query... works, but why did I put this here? IDK I'm tired.
+			// this will be used to convert address or zip to lat/lng and then center map accordingly
 			// sample format: address=1600+Amphitheatre+Parkway,+Mountain+View,+CA
+			// TO-DO: get key
 			var address = data.address +', '+data.city+', '+data.state;
 			var addressArray = address.split(' ');
 			var addressParam = addressArray.join('+');
 			queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?address='+addressParam;
-			$.ajax({
-			      url: queryURL,
-			      method: "GET"
-			    })
-			    .done(function(response) {
-			      console.log(response);
-			    })
+			// $.ajax({
+			// 	url: queryURL,
+			// 	method: "GET"
+			// })
+			// .done(function(response) {
+			// 	console.log(response);
+			// })
 		}
 
+		//create a marker for each location in database using the Place constructor and put it on the map
 		this.createMarkers = function(map) {
 			for (var key in locations){
 				if(!locations.hasOwnProperty(key)) continue;
 				var obj = locations[key];
-				// console.log( obj.name, obj.location, obj.address, obj.state, obj.zip, obj.url);
 				places.push(new Place(obj, map));
 			}
 		};
 
-
-		    //locations populated by snapshot in app.js
-	    //FB always returns nested objects, not arrays, so locations is an object now.
-	    //https://firebase.googleblog.com/2014/04/best-practices-arrays-in-firebase.html
-	    //save nested objects in array locally or rewrite maps code in order to access data in objects
-
-	    var map = new google.maps.Map(document.getElementById('googleMap'), {
-	      zoom: 10,
-	      //based on the user zip code and we will need to geo code to take the lat long
+		var map = new google.maps.Map(document.getElementById('googleMap'), {
+			zoom: 10,
+	      //TO-DO: Center map on user input
+	      //user will submit zip code or address, which is passed to geocode api. response provides lat/lng used to center map accordingly.
 	      center:  {"lat" : 32.760391, "lng" : -97.371262},
 	      mapTypeId: google.maps.MapTypeId.ROADMAP
-	    });
+	  });
 
-	    this.createMarkers(map);
+		this.createMarkers(map);
 
+	   	// refactored this code to use info from FB objects
 	    // var marker, i;
 
 	    // for (i = 0; i < locations.length; i++) {
@@ -81,14 +89,14 @@ function initMap(){
 	    //   // show photo
 	    // }
 
-	$(document).ready(function(){
-	  $('#myModal').on('shown.bs.modal', function(){
-	    google.maps.event.trigger(map, 'resize');
-	    map.setCenter(new google.maps.LatLng(32.760391, -97.371262));
-	  });
-	});
+	    $(document).ready(function(){
+	    	$('#myModal').on('shown.bs.modal', function(){
+	    		google.maps.event.trigger(map, 'resize');
+	    		map.setCenter(new google.maps.LatLng(32.760391, -97.371262));
+	    	});
+	    });
 
-});
+	});
 }
 
 //link index.html to firebase;
